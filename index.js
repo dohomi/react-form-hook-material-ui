c93ad6fcff5e611cc2ccabb8a6510e3b4e7717f1
Dominic Garms <djgarms@gmail.com>
2020-02-07 14:32:37 +0800

    package improvement

--- index.js
+++
@@ -1,8 +0,0 @@
-export { default as TextFieldElement } from './dist/TextFieldElement';
-export { default as CheckboxButtonGroup } from './dist/CheckboxButtonGroup';
-export { default as DatePickerElement } from './dist/DatePickerElement';
-export { default as FormContainer } from './dist/FormContainer';
-export { default as RadioButtonGroup } from './dist/RadioButtonGroup';
-export { default as SelectElement } from './dist/SelectElement';
-export { default as MultiSelectElement } from './dist/MultiSelectElement';
-export { default as PasswordElement } from './dist/PasswordElement';
